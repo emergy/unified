6ab4045af5b1f9b11663e3303b4546f4c3ad6359
Alex Emergy <alex.emergy@gmail.com>
2016-01-18 16:19:35 +0300

    init

---
+++ PhilipsTV/README.md
@@ -0,0 +1,3 @@
+Philips TV jointSPACE remote
+
+Change IP address in remote.lua file
---
+++ PhilipsTV/remote.lua
@@ -0,0 +1,108 @@
+local http = libs.http;
+local data = require("data");
+
+local url = "http://192.168.1.18:1925";
+
+-- Documentation
+-- https://github.com/unifiedremote/Docs
+
+-- Slider
+-- https://github.com/unifiedremote/Docs/blob/master/controls/slider.md
+
+-- http library
+-- https://github.com/unifiedremote/Docs/blob/master/libs/http.md
+
+update_volume();
+
+function update_volume()
+    http.get(url .. "/1/audio/volume", function (err, resp)
+        if (err) then return; end
+        print(resp);
+        local volume_data = data.fromjson(resp);
+        layout.vol.progress = volume_data.current;
+        layout.vol.progressmax = volume_data.max;
+        if (volume_data.muted) then
+            layout.mute.color = "red";
+        else
+            layout.mute.color = "";
+        end
+    end);
+end
+
+function send_key(key)
+    http.post(url .. "/1/input/key", '{"key":"' .. key .. '"}', function (err, resp)
+        if (err) then return; end
+        print(resp);
+    end);
+end
+
+actions.volume_change = function (progress)
+    http.post(url .. "/1/audio/volume", '{"muted":false,"current":' .. progress .. '"}', function (err, resp)
+        if (err) then return; end
+        print(resp);
+    end);
+end
+
+actions.volume_mute = function ()
+    send_key("Mute");
+    update_volume();
+end
+
+actions.volume_down = function ()
+    send_key("VolumeDown");
+    update_volume();
+end
+
+actions.volume_up = function ()
+    send_key("VolumeUp");
+    update_volume();
+end
+
+actions.screenoff = function ()
+    send_key("GreenColour");
+    os.sleep(2500);
+    for i = 1, 4 do
+        send_key("CursorUp");
+        os.sleep(200);
+    end
+    send_key("CursorDown");
+    os.sleep(500);
+    send_key("Confirm");
+end
+
+actions.back = function ()
+    send_key("Back");
+end
+
+actions.up = function ()
+    send_key("CursorUp");
+end
+
+actions.light = function ()
+    send_key("AmbilightOnOff");
+end
+
+actions.left = function ()
+    send_key("CursorLeft");
+end
+
+actions.enter = function ()
+    send_key("Confirm");
+end
+
+actions.right = function ()
+    send_key("CursorRight");
+end
+
+actions.options = function ()
+    send_key("Options");
+end
+
+actions.down = function ()
+    send_key("CursorDown");
+end
+
+actions.format = function ()
+    send_key("Viewmode");
+end
+
